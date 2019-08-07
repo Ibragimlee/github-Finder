468c6f09bee193c9a6b138c88f65b4eb92658536
Adil Ibrahimli <43700563+Ibragimlee@users.noreply.github.com>
2019-08-07 11:00:42 +0400

    Add files via upload

---
+++ githubFinder/js/app.js
@@ -0,0 +1,33 @@
+// init Girhub
+const github = new Github;
+const ui = new UI;
+
+// Search input
+const searchUser = document.querySelector('#searchUser');
+
+// search input event listener
+searchUser.addEventListener('keyup', (e) => {
+    // Get input text
+    const userText = e.target.value;
+
+    if(userText != ''){
+        // make http call
+
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === 'Not Found'){
+                // Show alert
+                ui.showAlert('User not found', 'alert alert-danger');
+            }
+            else{
+                // Show profile
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+
+            }
+        })
+    } else{
+        // Clear profile
+        ui.clearProfile();
+    }
+})
